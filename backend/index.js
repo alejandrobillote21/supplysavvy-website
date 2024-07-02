3a80229cdbe7079ae652db0eb255c01e50e28e36
alejandrobillote21 <108616723+alejandrobillote21@users.noreply.github.com>
2024-07-02 23:10:15 +0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,44 +4,47 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
-// Load environment variables from .env file
-dotenv.config();
+// Utils
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
+import uploadRoutes from "./routes/uploadRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-const mongoURI = process.env.MONGO_URI;
-const jwtSecret = process.env.JWT_SECRET;
+dotenv.config();
 const port = process.env.PORT || 5000;
 
-// Initialize Express app
+connectDB();
+
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-// Allow requests from specific origin with required methods
-const corsOptions = {
-  origin: "https://supplysavvy-website-frontend.vercel.app",
-  methods: ["POST", "GET", "PUT", "DELETE"],
-  credentials: true, // Enable credentials (cookies, authorization headers) if needed
-};
-
-app.use(cors(corsOptions));
-
-// Serve static files (if applicable)
-const __dirname = path.resolve();
-app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+app.use(cors(
+  {
+    origin: ["https://supplysavvy-website-frontend.vercel.app"],
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true
+  }
+));
 
 // Define your API routes
-app.use("/api/users", require("./routes/userRoutes"));
-app.use("/api/category", require("./routes/categoryRoutes"));
-app.use("/api/products", require("./routes/productRoutes"));
-app.use("/api/upload", require("./routes/uploadRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes"));
+app.use("/api/users", userRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes);
 
 // Example API endpoint
 app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
+
+// Serve static files (if applicable)
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 // Root route handler
 app.get("/", (req, res) => {
@@ -54,7 +57,6 @@ app.use((err, req, res, next) => {
   res.status(500).send('Something broke!');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
