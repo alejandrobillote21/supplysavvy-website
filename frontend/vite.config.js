9ca0990425377db91388119c243ca8f85a53a2d3
alejandrobillote21 <108616723+alejandrobillote21@users.noreply.github.com>
2024-07-02 23:58:47 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "supplysavvy-website-api.vercel.app",
+      "/uploads/": "supplysavvy-website-api.vercel.app",
     },
   },
 });
